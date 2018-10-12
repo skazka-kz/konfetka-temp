309bdef66930f6fee3f2a4739488ff5e4eaa3c11
Slava Bezgachev <bezgachev@gmail.com>
2018-10-12 13:23:15 +0600

    Updates

---
+++ precache-manifest.7120f302bd19e7cbc3ae27ad617d2593.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "95104db539c3361740af79af307bdeee",
+    "url": "/static/media/logo.95104db5.png"
+  },
+  {
+    "revision": "5eb4df79d4fc3621c01f0187d3865411",
+    "url": "/static/media/candy_pattern.5eb4df79.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f74561c596d9f49628cc",
+    "url": "/static/js/main.f74561c5.chunk.js"
+  },
+  {
+    "revision": "094ee21c68d8097dfe09",
+    "url": "/static/js/1.094ee21c.chunk.js"
+  },
+  {
+    "revision": "f74561c596d9f49628cc",
+    "url": "/static/css/main.b864aa23.chunk.css"
+  },
+  {
+    "revision": "b27968fca952147a74ec3d2c347c0571",
+    "url": "/index.html"
+  }
+];

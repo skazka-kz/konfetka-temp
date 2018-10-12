526d175618ecba05ab1438346f8724d7fbc35699
Slava Bezgachev <bezgachev@gmail.com>
2018-10-12 11:52:34 +0600

    Logo section done

--- src/components/presentation/Header/LogoContainer.js
+++ src/components/presentation/Header/LogoContainer.js
@@ -1,7 +1,21 @@
 import styled from "styled-components";
 
 const LogoContainer = styled.div`
+  background-color: #fe6321;
+  display: flex;
+  justify-content: center;
+  height: 100px;
+  border-bottom: 3px solid #70401C;
 
+  img {
+    max-width: 85%;
+    margin: auto;
+    max-height: 100%;
+  }
+
+  @media (min-width: 768px) {
+    height: 150px;
+  }
 `;
 
 export default LogoContainer;
